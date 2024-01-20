2f387b98cd557390f8ccf3fb39f6b9f8a7ab9baa
srajansohani <srajansohani999@gmail.com>
2024-01-21 01:29:20 +0530

    Ready to Go

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+    const {commits} = payload;
+    const changedFiles = commits.reduce((acc, commit) => {
+        return acc.concat(commit?.added, commit?.modified, commit?.removed);
+    }, []);
+    console.log(changedFiles)
 } catch (error) {
   core.setFailed(error.message);
 }
